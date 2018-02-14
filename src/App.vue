53e9ceab8f6ecc24256f7a4b5ec669b1bb9a91a9
Jaskaran <wp.jaskaranakalra@gmail.com>
2018-02-15 01:32:38 +0530

    complete shape with flip over, flip back, flip right, flip left, flip up, flip down

---
+++ src/App.vue
@@ -0,0 +1,240 @@
+<template>
+  <div id="app">
+    <div class="ui padded segment">
+      <h2 class="ui header">Shape</h2>
+      <p>A shape is a three dimensional object including any flat content as a side.</p>
+      <vue-semantic-ui-shape ref="shapes">
+        <div class="sides">
+          <div class="side">
+            <div class="ui card">
+              <div class="image">
+                <img src="http://semantic-ui.com/images/avatar/large/steve.jpg">
+              </div>
+              <div class="content">
+                <div class="header">Steve Jobs</div>
+              </div>
+            </div>
+          </div>
+          <div class="side">
+            <div class="ui card">
+              <div class="image">
+                <img src="http://semantic-ui.com/images/avatar/large/stevie.jpg">
+              </div>
+              <div class="content">
+                <div class="header">Stevie Feliciano</div>
+              </div>
+            </div>
+          </div>
+           <div class="side">
+            <div class="ui card">
+              <div class="image">
+                <img src="http://semantic-ui.com/images/avatar/large/steve.jpg">
+              </div>
+              <div class="content">
+                <div class="header">Steve Works</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </vue-semantic-ui-shape>
+      <div class="ui hidden clearing divider"></div>
+      <div class="ui icon buttons">
+        <button class="ui button" @click="flipLeft('shapes')">
+          <i class="left chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipUp('shapes')">
+          <i class="up chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipDown('shapes')">
+          <i class="down chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipRight('shapes')">
+          <i class="right chevron icon"></i>
+        </button>
+      </div>
+      <div class="ui icon buttons">
+        <div class="ui button" @click="flipBack('shapes')"><i class="retweet icon"></i></div>
+        <div class="ui button" @click="flipOver('shapes')"><i class="flipped retweet icon"></i></div>
+      </div>
+    </div>
+    <div class="ui padded segment">
+      <h2>Cube</h2>
+      <p>A cube shape is formatted so that each side is the face of a cube</p>
+      <vue-semantic-ui-shape class="cube" ref="cubes">
+        <div class="sides">
+          <div class="side">
+            <div class="content">
+              <div class="center">
+                1
+              </div>
+            </div>
+          </div>
+          <div class="side">
+            <div class="content">
+              <div class="center">
+                2
+              </div>
+            </div>
+          </div>
+          <div class="side">
+            <div class="content">
+              <div class="center">
+                3
+              </div>
+            </div>
+          </div>
+          <div class="side">
+            <div class="content">
+              <div class="center">
+                4
+              </div>
+            </div>
+          </div>
+        </div>
+      </vue-semantic-ui-shape>
+      <div class="ui hidden clearing divider"></div>
+      <div class="ui icon buttons">
+        <button class="ui button" @click="flipLeft('cubes')">
+          <i class="left chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipUp('cubes')">
+          <i class="up chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipDown('cubes')">
+          <i class="down chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipRight('cubes')">
+          <i class="right chevron icon"></i>
+        </button>
+      </div>
+      <div class="ui icon buttons">
+        <div class="ui button" @click="flipBack('cubes')"><i class="retweet icon"></i></div>
+        <div class="ui button" @click="flipOver('cubes')"><i class="flipped retweet icon"></i></div>
+      </div>
+    </div>
+    <div class="ui padded segment">
+      <h2>Form</h2>
+      <p>A form shape is formatted to allow for sides of form to be displayed</p>
+      <vue-semantic-ui-shape class="form" ref="form">
+        <div class="sides">
+          <div class="ui side">
+            <form class="ui form" style="height:241px;width:219px;">
+              <div class="field">
+                <label>First Name</label>
+                <input style="height:38px;width:219px;" type="text" name="first-name" placeholder="First Name">
+              </div>
+              <div class="field">
+                <label>Last Name</label>
+                <input style="height:38px;width:219px;" type="text" name="last-name" placeholder="Last Name">
+              </div>
+              <div class="field">
+                <div class="ui checkbox">
+                  <input style="height:38px;width:219px;" type="checkbox" tabindex="0" class="hidden">
+                  <label>I agree to the Terms and Conditions</label>
+                </div>
+              </div>
+              <button class="ui button" type="submit">Submit</button>
+            </form>
+          </div>
+          <div class="ui side">
+            <div class="ui form" style="height:75px;width:568px;">
+              <div class="fields">
+                <div class="field">
+                  <label>First name</label>
+                  <input style="height:38px;width:219px;" type="text" placeholder="First Name">
+                </div>
+                <div class="field">
+                  <label>Middle name</label>
+                  <input style="height:38px;width:219px;" type="text" placeholder="Middle Name">
+                </div>
+                <div class="field">
+                  <label>Last name</label>
+                  <input style="height:38px;width:219px;" type="text" placeholder="Last Name">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </vue-semantic-ui-shape>
+      <div class="ui hidden clearing divider"></div>
+      <div class="ui icon buttons">
+        <button class="ui button" @click="flipLeft('form')">
+          <i class="left chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipUp('form')">
+          <i class="up chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipDown('form')">
+          <i class="down chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipRight('form')">
+          <i class="right chevron icon"></i>
+        </button>
+      </div>
+      <div class="ui icon buttons">
+        <div class="ui button" @click="flipBack('form')"><i class="retweet icon"></i></div>
+        <div class="ui button" @click="flipOver('form')"><i class="flipped retweet icon"></i></div>
+      </div>
+    </div>
+    <div class="ui padded segment">
+      <h2>Text</h2>
+      <p>A text shape is formatted to allow for sides of text to be displayed</p>
+      <vue-semantic-ui-shape class="text" ref="text">
+        <div class="sides">
+          <div class="ui header side active">Did you know? This side starts visible.</div>
+          <div class="ui header side">Help, its another side!</div>
+          <div class="ui header side">This is the last side</div>
+        </div>
+      </vue-semantic-ui-shape>
+      <div class="ui hidden clearing divider"></div>
+      <div class="ui icon buttons">
+        <button class="ui button" @click="flipLeft('text')">
+          <i class="left chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipUp('text')">
+          <i class="up chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipDown('text')">
+          <i class="down chevron icon"></i>
+        </button>
+        <button class="ui button" @click="flipRight('text')">
+          <i class="right chevron icon"></i>
+        </button>
+      </div>
+      <div class="ui icon buttons">
+        <div class="ui button" @click="flipBack('text')"><i class="retweet icon"></i></div>
+        <div class="ui button" @click="flipOver('text')"><i class="flipped retweet icon"></i></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueSemanticUiShape from './components/VueSemanticUiShape.vue';
+export default {
+  name: 'app',
+  components: {
+    VueSemanticUiShape
+  },
+  methods: {
+    flipOver(ref){
+      this.$refs[ref].flipOver();
+    },
+    flipBack(ref){
+      this.$refs[ref].flipBack();
+    },
+    flipLeft(ref){
+      this.$refs[ref].flipLeft();
+    },
+    flipRight(ref){
+      this.$refs[ref].flipRight();
+    },
+    flipUp(ref){
+      this.$refs[ref].flipUp();
+    },
+    flipDown(ref){
+      this.$refs[ref].flipDown();
+    }
+  }
+}
+</script>
---
+++ src/components/VueSemanticUiShape.vue
@@ -0,0 +1,289 @@
+<template>
+  <div class="ui shape"><slot></slot></div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        prevIndex: 0,
+        index: 0,
+        sidesEl: '',
+        sides: [],
+        nextDirs: ['down','right','flip-right'],
+        prevDirs: ['up','left','flip-left'],
+        shapeDim: {
+          h: 0,
+          w: 0,
+        },
+        curDir: '',
+        hHalf: 0,
+        wHalf: 0,
+        flipMode: false,
+        animating: false,
+        sidesDim: []
+      }
+    },
+    computed: {
+      totalSides(){
+        return this.sides.length;
+      },
+      isAnimating(){
+        return this.animating;
+      },
+      isForm(){
+        return this.$el.classList.contains('form');
+      }
+    },
+    methods: {
+      flipOver(){
+        this.changeSide('flip-left');
+      },
+      flipBack(){
+        this.changeSide('flip-right');
+      },
+      flipLeft(){
+        this.changeSide('left');
+      },
+      flipRight(){
+        this.changeSide('right');
+      },
+      flipUp(){
+        this.changeSide('up');
+      },
+      flipDown(){
+        this.changeSide('down');
+      },
+      countSides(){
+        this.sides   = this.$el.querySelectorAll('.side');
+
+        this.sidesEl = this.$el.querySelector('.sides');
+
+        if(! this.sidesEl){
+          console.error('Sides Element not found !');
+          return;
+        }
+
+        if(! this.sides.length ){
+          console.error('Please add sides to use it !');
+          return;
+        }
+
+        this.addClass(this.sides[0], ['active']);
+
+        if( this.isForm ){
+          this.setSidesDim();
+        }
+
+        setTimeout(this.setShapeDim, 2000);
+      },
+      setSidesDim(){
+        this.sides.forEach(row => {
+
+          let form = row.querySelector('.form');
+
+          let el = form.querySelector('input');
+          
+          let h = el.style.height ? el.style.height : 0;
+
+          let w = el.style.height ? el.style.width : 0;
+
+          el.style.minHeight = h;
+
+          el.style.minWidth = w;
+
+          this.sidesDim.push({h: ~~form.style.height.replace('px',''), w: ~~form.style.width.replace('px','')});
+        });
+      },
+      changeSide(dir){
+        if(this.animating ) return;
+
+        this.animating = true;
+
+        if( dir.includes('flip-') ){
+          this.flipMode = true;
+
+          dir = dir.replace('flip-','');
+        }else{
+          this.flipMode = false;
+        }
+
+        this.curDir = dir;
+
+        this.prevIndex = this.index;
+
+        let pos = this.index;
+
+        pos += ( ~~this.nextDirs.includes(dir) || -1 );
+
+        this.index = ( pos < 0 ) ? this.totalSides - 1 : pos % this.totalSides;
+
+        this.setActiveSide();
+      },
+      setActiveSide(){
+        this.setShapeDim();
+
+        this.setDimensions();
+
+        this.hidePrevSide();
+
+        this.showCurSide();
+
+        this.$el.classList.add('animating');
+
+        this.setAnimationOnShapes();
+      },
+      setDimensions(){
+        this.$el.style.height = this.shapeDim.h + 'px';
+        this.$el.style.width = this.shapeDim.w + 'px';
+      },
+      setAnimationOnShapes(){
+        let props = null;
+
+        if( this.flipMode ){
+          switch(this.curDir){
+            case 'left':
+              props = ("translateX(0px) rotateY(180deg)");
+              break;
+
+            case 'right':
+              props = ("translateX(0px) rotateY(-180deg)");
+              break;
+          }          
+        }else{
+          switch (this.curDir){
+            case 'right':
+              props = ("translateX(0) translateZ(-"+this.wHalf+"px) rotateY(-90deg)");
+              break;
+
+            case 'left':
+              props =  ("translateX(0) translateZ(-"+this.wHalf+"px) rotateY(90deg)");
+              break;
+
+            case 'down':
+              props = ("translateY(0) translateZ(-"+this.hHalf+"px) rotateX(90deg)");
+              break;
+
+            case 'up': 
+              props = ("translateY(0) translateZ(-"+this.hHalf+"px) rotateX(-90deg)");
+              break;
+          }
+        }
+
+        this.sidesEl.style.transform = props;
+      },
+      hidePrevSide(){
+        let prevSide = this.sides[this.prevIndex];
+
+        this.addClass(prevSide, ['hidden']);
+
+        let props = null;
+
+        if(this.flipMode){
+          switch(this.curDir){
+            case 'right':
+            case 'left':
+              props = ("rotateY(0deg)");
+              break;
+          }
+        }else{
+          switch(this.curDir){
+            case 'right':
+            case 'left':
+              props = ("rotateY(0deg) translateZ("+this.wHalf+"px)");
+              break;
+
+            case 'up':
+            case 'down':
+              props = ("rotateY(0deg) translateZ("+this.hHalf+"px)");
+              break;
+          }
+        }
+
+        prevSide.style.transform = props;
+      },
+      showCurSide(){
+        let curSide = this.sides[this.index];
+
+        this.addClass(curSide, ['animating']);
+
+        let styles = {};
+
+        if( this.flipMode ){
+           switch(this.curDir){
+            case 'right':
+            case 'left':
+              styles = {left: '0px', transform: ("rotateY(-180deg)")};
+              break;
+          }
+        }else{
+          switch(this.curDir){
+            case 'right':
+              styles = {left: '0px', transform: ("rotateY(90deg) translateZ("+this.wHalf+"px)")};
+              break;
+
+            case 'left':
+              styles = {left: '0px', transform: ("rotateY(-90deg) translateZ("+this.wHalf+"px)")};
+              break;
+
+            case 'up':
+              styles = {top: '0px', transform: ("rotateX(90deg) translateZ("+this.hHalf+"px)")};
+              break;
+
+            case 'down':
+               styles = {top: '0px', transform: ("rotateX(-90deg) translateZ("+this.hHalf+"px)")};
+              break;
+          }
+        }   
+
+        for (let s in styles){
+          if (styles.hasOwnProperty(s)) {
+            curSide.style[s] = styles[s];
+          }
+        }
+
+        this.removeAnimation();
+      },
+      removeAnimation(){
+        setTimeout(() =>{
+          let curSide = this.sides[this.index], prevSide = this.sides[this.prevIndex];
+          
+          prevSide.classList.remove('active', 'hidden');
+
+          this.addClass(curSide, ['active']);
+
+          this.$el.removeAttribute('style');
+
+          this.sidesEl.removeAttribute('style');
+
+          prevSide.removeAttribute('style');
+
+          curSide.removeAttribute('style');
+
+          this.removeClass(this.$el, ['animating']);
+
+          this.removeClass(curSide, ['animating']);
+
+          this.animating = false;
+        }, 1000);
+      },
+      addClass(elm, classList){
+        elm.classList.add(classList.join(','));
+      },
+      removeClass(elm, classList){
+        elm.classList.remove(classList.join(','));
+      },
+      setShapeDim(){
+        this.shapeDim = {
+          h: this.isForm ? this.sidesDim[this.index].h : this.$el.scrollHeight,
+          w: this.isForm ? this.sidesDim[this.index].w : this.$el.scrollWidth
+        };
+
+        this.hHalf = this.shapeDim.h/2;
+        this.wHalf = this.shapeDim.w/2;
+      },
+    },
+    mounted(){
+      this.countSides();
+    }
+  }
+</script>
